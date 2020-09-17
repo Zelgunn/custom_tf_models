a8a3c4a01c56036d06cb1a4edcadc8a5383aaa4f
Valentin Durand de Gevigney <valentin.durand.de.g@gmail.com>
2020-09-17 10:24:15 +0200

    LED.py : description_energy_loss is now only weighted when trying to reconstruct noisy inputs. Added a parameter to control whether to reconstruct noise in inputs or not.

--- description_length/LED.py
+++ description_length/LED.py
@@ -25,6 +25,7 @@ class LED(AE):
                  add_binarization_noise_to_mask=True,
                  description_energy_loss_lambda=1e-2,
                  noise_stddev=0.1,
+                 reconstruct_noise=False,
                  seed=None,
                  **kwargs
                  ):
@@ -39,6 +40,7 @@ class LED(AE):
         self.add_binarization_noise_to_mask = add_binarization_noise_to_mask
         self.description_energy_loss_lambda = description_energy_loss_lambda
         self.noise_stddev = noise_stddev
+        self.reconstruct_noise = reconstruct_noise
         self.seed = seed
 
         self.description_energy_model = self._make_description_model()
@@ -139,28 +141,37 @@ class LED(AE):
     @tf.function
     def description_energy_loss(self, description_energy: tf.Tensor, noise_factor: tf.Tensor) -> tf.Tensor:
         description_energy = reduce_mean_from(description_energy, start_axis=1)
-        weights = tf.constant(1.0) - tf.square(noise_factor)
-        description_energy = description_energy * weights
+        if self.self.reconstruct_noise:
+            weights = tf.constant(1.0) - tf.square(noise_factor)
+            description_energy = description_energy * weights
         description_energy = tf.reduce_mean(description_energy)
         return description_energy
 
     @tf.function
     def compute_loss(self, inputs) -> Dict[str, tf.Tensor]:
+        # region Noisy inputs
         batch_size = tf.shape(inputs)[0]
         noise_factor = tf.random.uniform([batch_size], maxval=1.0, seed=self.seed)
         noise = tf.random.normal(tf.shape(inputs), stddev=self.noise_stddev, seed=self.seed)
         noisy_inputs = inputs + noise * expand_dims_to_rank(noise_factor, inputs)
+        # endregion
 
+        # region Forward
         encoded = self.encoder(noisy_inputs)
         description_energy = self.description_energy_model(encoded)
         description_mask = self.get_description_mask(description_energy)
         encoded *= description_mask
         outputs = self.decode(encoded)
+        # endregion
 
-        reconstruction_loss = self.reconstruction_loss(inputs, outputs)
+        # region Loss
+        reconstruction_target = noisy_inputs if self.reconstruct_noise else inputs
+        reconstruction_loss = self.reconstruction_loss(reconstruction_target, outputs)
         description_energy_loss = self.description_energy_loss(description_energy, noise_factor)
         loss = reconstruction_loss + self._description_energy_loss_lambda * description_energy_loss
+        # endregion
 
+        # region Metrics
         description_length = tf.reduce_mean(tf.stop_gradient(description_mask))
 
         metrics = {
@@ -169,6 +180,7 @@ class LED(AE):
             "description_energy_loss": description_energy_loss,
             "description_length": description_length,
         }
+        # endregion
 
         return metrics
 
@@ -186,6 +198,7 @@ class LED(AE):
             "add_binarization_noise_to_mask": self.add_binarization_noise_to_mask,
             "description_energy_loss_lambda": self.description_energy_loss_lambda,
             "noise_stddev": self.noise_stddev,
+            "reconstruct_noise": self.reconstruct_noise,
             "seed": self.seed,
         }
 
